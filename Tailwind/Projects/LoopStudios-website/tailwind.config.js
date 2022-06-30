a2fde78b5b592e12e6928ad3225f4b6dacb12b03
nityam0213 <nityam0213@gmail.com>
2022-06-30 22:38:13 +0530

    Project2 DONE

---
+++ Tailwind/Projects/LoopStudios-website/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Josefin Sans", "sans-serif"],
+        alata: ["Alata"],
+      },
+      letterSpacing: {
+        widest: ".3em",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
